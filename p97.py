fe9481051d7e4532013c61b59a3e1fbca481fc3d
Aadi123456 <73757260+Aadi123456@users.noreply.github.com>
2021-06-19 11:07:38 -0400

    Add files via upload

---
+++ p97.py
@@ -0,0 +1,17 @@
+import randomNumber
+randomNumber.radiant(1,9)
+
+chances = 5
+
+while chances < 5:
+if not chances < 5:
+    print("You Lose!!! The number is", randomNumber)
+
+
+
+if guess == randomNumber:
+    print("Congratulations You Won!!!")
+    break
+if guess not == randomNumber:
+    print("You have guessed the incorrect number")
+    break
